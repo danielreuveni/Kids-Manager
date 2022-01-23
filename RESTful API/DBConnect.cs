3b831d856f57fc6982f744a42179d6fb94b94da4
Daniel Reuveni <73079447+danielreuveni@users.noreply.github.com>
2022-01-23 08:30:30 +0200

    Add files via upload

---
+++ RESTful API/DBConnect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTful_API
+{
+    public class DBConnect
+    {
+        private static SqlConnection NewCon;
+        private static stringconStr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;    
+        public static SqlConnection getConnection()
+        {
+            NewCon = new SqlConnection(conStr);
+            return NewCon;
+        }
+        public DBConnect()
+        {
+
+        }
+
+    }
+}
